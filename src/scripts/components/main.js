9bd39b44f93f8a4d6a1aa6f96d2e22dbfde4b45d
Eric Wooley <Eric.Wooley@rosetta.com>
2015-09-09 17:25:21 -0700

    updated videos

--- src/scripts/components/main.js
+++ src/scripts/components/main.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import ReactInStyle from 'react-in-style';
 import MediaStreamer from '../libs/media-streamer';
 const vid1 = require('../../images/devstories.webm');
-const vid2 = require('../../images/swirls.webm');
+const vid2 = require('../../images/eric1-2.webm');
+const vid3 = require('../../images/eric2-2.webm');
+const vid4 = require('../../images/project-fi.webm');
 console.log(vid1);
 class Main extends Component {
   constructor() {
@@ -45,6 +47,10 @@ class Main extends Component {
         <button className="vid-1-src" onClick={this.setSrc(vid1)}>SRC: Video 1</button><hr />
         <button className="vid-2" onClick={this.appendVideo(vid2)}>Video 2</button><br />
         <button className="vid-2-src" onClick={this.setSrc(vid2)}>SRC: Video 2</button><hr />
+        <button className="vid-3" onClick={this.appendVideo(vid3)}>Video 3</button><br />
+        <button className="vid-3-src" onClick={this.setSrc(vid3)}>SRC: Video 3</button><hr />
+        <button className="vid-4" onClick={this.appendVideo(vid4)}>Video 4</button><br />
+        <button className="vid-4-src" onClick={this.setSrc(vid4)}>SRC: Video 4</button><hr />
         Dynamic Src: {this.state.dynamicSrc ? 'true' : 'false' }
         <hr />
         <video controls ref="video"/>
